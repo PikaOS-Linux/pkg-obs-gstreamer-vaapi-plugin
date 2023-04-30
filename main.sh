c48efc6bef852e64188556abbdba4d9d0ada2582
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-04-30 20:28:41 +0300

    update to lunar

---
+++ main.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -e
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://github.com/fzwoch/obs-vaapi
+cp -rvf ./debian ./obs-vaapi/
+cd ./obs-vaapi
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
