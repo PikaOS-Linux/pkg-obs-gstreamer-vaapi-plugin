1f8385f0a7482f56015ccc68e3aff28628b4a460
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:00:29 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 set -e
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
+
 # Clone Upstream
 git clone https://github.com/fzwoch/obs-vaapi
 cp -rvf ./debian ./obs-vaapi/
